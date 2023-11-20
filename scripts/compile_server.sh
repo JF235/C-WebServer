b84f499661cfb6531f70b9f5481d6ec16c7a6ad7
João <jfcm.pipe@gmail.com>
2023-11-20 16:12:46 -0300

    Editando o tratamento do webspace.

--- scripts/compile_server.sh
+++ scripts/compile_server.sh
@@ -10,10 +10,11 @@ gcc *.c -o ../server -lfl -lpthread -lcrypt -Wall -Wextra -g
 
 # Avisar que o webspace não está pronto
 
-caminho_arquivo="../web/meu-webspace"
+cd ..
+caminho_arquivo="web/meu-webspace"
 
 if [ -e "$caminho_arquivo" ]; then
     echo "Tudo pronto."
 else
-    echo -e "O webspace não está montado.\nExecute o script setup_webspace.sh para montar."
+    ./scripts/setup_webspace.sh
 fi
