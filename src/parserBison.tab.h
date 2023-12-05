8f33771bbf888b0b3d1ad522d19ccaeb6babd1dc
Joao Felipe Contreras De Moraes <ra174140@le27-11.grad.fee.unicamp.br>
2023-12-05 14:38:21 -0300

    Melhorando ainda mais os webspaces

--- src/parserBison.tab.h
+++ src/parserBison.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,8 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
 #ifndef YY_YY_PARSERBISON_TAB_H_INCLUDED
 # define YY_YY_PARSERBISON_TAB_H_INCLUDED
@@ -44,16 +45,21 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    COMMA = 258,
-    COLONS = 259,
-    WORD = 260,
-    HTTP_METHOD = 261
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    COMMA = 258,                   /* COMMA  */
+    COLONS = 259,                  /* COLONS  */
+    WORD = 260,                    /* WORD  */
+    HTTP_METHOD = 261              /* HTTP_METHOD  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
@@ -66,7 +72,7 @@ union YYSTYPE
     struct OptionList *optionList;
     struct CommandList *commandList;
 
-#line 70 "parserBison.tab.h"
+#line 76 "parserBison.tab.h"
 
 };
 typedef union YYSTYPE YYSTYPE;
@@ -77,6 +83,8 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE yylval;
 
+
 int yyparse (void);
+
 
 #endif /* !YY_YY_PARSERBISON_TAB_H_INCLUDED  */
